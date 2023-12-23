74dbae0582f867effc95a56cde8f0073442ca10f
Rahmadiyan Muhammad <81454957+rianying@users.noreply.github.com>
2023-12-23 09:56:20 +0700

    .

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,14 @@
-const audio = document.querySelector('audio');
+windows.addEventListener('load', () => {
+    const audio = document.querySelector('audio');
     const body = document.body;
 
-audio.addEventListener('play', () => {
-        boody.classList.add('play-music');
-    }
-);
+    audio.addEventListener('play', () => {
+        body.classList.add('play-music');
+    });
 
-audio.addEventListener('pause', () => {
-    body.classList.remove('play-music');
-    }
-);
+    audio.addEventListener('pause', () => {
+        body.classList.remove('play-music');
+    });
+
+    audio.play();
+})
